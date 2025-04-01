e3373dff21398d8c927ab45b395b352371156954
Cronan99 <albincronmark@gmail.com>
2025-04-01 10:40:08 +0200

    Fixed: Header now shows relevant button based on logged in user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,37 @@ import React from 'react';
 import styles from '../styles/Header.module.css';
 import Button from './Button';
 import logoImage from '../assets/logo_in1.jpg'; // Adjust the path to your logo image
+import { getProtectedData, logout } from '../services/authApi';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Header = ({ onProfileClick, onLogoutClick, onSubcriptionClick }) => {
+const Header = () => {
+
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        await getProtectedData();
+        setIsLoggedIn(true);
+      } catch (error) {
+        setIsLoggedIn(false);
+      }
+    };
+    checkLoginStatus();
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      await logout(); // Call the API to log out
+      setIsLoggedIn(false); // Update state
+      navigate('/login'); // Redirect to login page
+    } catch (error) {
+      console.error('Logout failed:', error);
+    }
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
@@ -13,26 +42,28 @@ const Header = ({ onProfileClick, onLogoutClick, onSubcriptionClick }) => {
           </div>
         
         <div className={styles.buttonsContainer}>
-        <Button
-            onClick={onSubcriptionClick}
-            variant="primary"
-            >
-              Subscribe
-            </Button>
-
-          <Button 
-            onClick={onProfileClick}
-            variant="primary"
-          >
-            Profile
-          </Button>
-
-          <Button 
-            onClick={onLogoutClick}
-            variant="danger"
-          >
-            Logout
-          </Button>
+        {isLoggedIn ? (
+            <>
+              <Button onClick={() => navigate('/subscription')} variant="primary">
+                Subscribe
+              </Button>
+              <Button onClick={() => navigate('/profile-page')} variant="primary">
+                Profile
+              </Button>
+              <Button onClick={handleLogout} variant="danger">
+                Logout
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button onClick={() => navigate('/login')} variant="primary">
+                Login
+              </Button>
+              <Button onClick={() => navigate('/register-user')} variant="primary">
+                Register
+              </Button>
+            </>
+          )}
         </div>
       </div>
     </header>
